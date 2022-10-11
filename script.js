2c0b100272d3be1e1c1eb58dd8545aae6414eb61
epueque rocio <epuequerocio@gmail.com>
2022-10-11 17:06:45 -0300

    abro region de nombre suma

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ console.log(nombre);
 console.log(nombre2);
 console.log(dni);
 //#endregion nombres
+
+//#region suma
+
+function suma(num1, num2) {
+    let k = num1 + num2;
+    return k;
+}
+let num1 = 20;
+let num2 = 20;
+console.log(suma(num1, num2));
+//#endregion suma
