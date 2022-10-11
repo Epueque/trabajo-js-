31e7105d70f5d244924e0795c7e502cf886c6f35
epueque rocio <epuequerocio@gmail.com>
2022-10-11 17:05:12 -0300

    abro region con nombre nombres

--- script.js
+++ script.js
@@ -2,4 +2,10 @@
 const nombre = `Rocio`;
 let nombre2 = "Tomas";
 var dni = '59677854';
-//#endregion
+//#endregion variables
+
+//#region nombres
+console.log(nombre);
+console.log(nombre2);
+console.log(dni);
+//#endregion nombres
